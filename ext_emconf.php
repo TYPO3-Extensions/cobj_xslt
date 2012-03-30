29c93b91167603e2a993924ff636022b7083a470
schrade <schrade@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-03-30 21:34:05 +0000

    Release of 1.0.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cobj_xslt/trunk@60144 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "cobj_xslt".
 #
-# Auto generated 04-01-2012 17:54
+# Auto generated 30-03-2012 23:27
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -12,7 +12,7 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'XSLT Content Object',
-	'description' => 'Extends tslib_cObj with a new cObject XSLT for doing transformations on XML input with TypoScript.',
+	'description' => 'Extends tslib_cObj with a new cObject XSLT for flexible XML transformations with XSL and TypoScript.',
 	'category' => 'fe',
 	'author' => 'Torsten Schrade',
 	'author_email' => 'schradt@uni-mainz.de',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author_company' => 'Academy of Sciences and Literature | Mainz',
-	'version' => '0.0.0',
+	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.2.0-0.0.0',
@@ -37,10 +37,10 @@ $EM_CONF[$_EXTKEY] = array(
 		),
 		'conflicts' => array(
 		),
-		'suggests' => array(	
+		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:3:{s:28:"class.tx_cobj_headerdata.php";s:4:"625d";s:12:"ext_icon.gif";s:4:"bbb3";s:17:"ext_localconf.php";s:4:"58b2";}',
+	'_md5_values_when_last_written' => 'a:7:{s:22:"class.tx_cobj_xslt.php";s:4:"8a1c";s:16:"ext_autoload.php";s:4:"6f59";s:12:"ext_icon.gif";s:4:"08dd";s:17:"ext_localconf.php";s:4:"90d0";s:14:"doc/README.txt";s:4:"1e34";s:14:"doc/manual.sxw";s:4:"27fb";s:13:"res/t3rss.xsl";s:4:"2530";}',
 	'suggests' => array(
 	),
 );
